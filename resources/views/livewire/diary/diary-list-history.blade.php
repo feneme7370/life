f97f2927a488bff29881b68547c3766a828c5bec
feneme7370 <marascofederico95@gmail.com>
2026-02-03 21:11:44 -0300

    images diaries

--- resources/views/livewire/diary/diary-list-history.blade.php
+++ resources/views/livewire/diary/diary-list-history.blade.php
@@ -7,6 +7,17 @@
                 <p><span class="font-bold"></span>{{ $diary->day }} - {{ $humor_status[$diary->humor] ?? 'Desconocido'}}</p>
                 <p class="text-sm my-1">{{ $diary->title }}</p>
                 <p><span class="text-xs">{!! $diary->content !!}</span></p>
+
+            <!-- Mostrar imágenes guardadas -->
+            @if ($diary->images->count())
+                <div class="flex flex-wrap gap-1 mt-5 justify-center">
+                    @foreach ($diary->images as $image)
+                        <div class="relative group">
+                            <img src="{{ asset($image->path) }}" class="rounded-lg shadow h-16 w-16 md:w-32 md:h-32 object-cover" />
+                        </div>
+                    @endforeach
+                </div>
+            @endif
                 
                 <p>-------------------------------------------------------------------------------------------</p>
             </div>
